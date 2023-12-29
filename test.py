292244305a19b6dd4107eb37407cfa46ee9d9e1c
moritzSchn16 <moritz86.ms@gmail.com>
2023-12-29 20:00:09 +0100

    streamlit_app 1.2.1

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import gspread
 
 def main():
     st.title("Text Display App")
@@ -17,6 +19,21 @@ def main():
     else:
         # Display the input in the sidebar
         st.sidebar.write("You entered:", user_input)
+
+
+
+    # Open the Google Sheet by title or URL (no authentication needed for public sheets)
+    sheet = gspread.open("https://docs.google.com/spreadsheets/d/1Qh5L61LQPMMuhn6YsaAbVBZnwlYl8PJxKzSxTnjXwiA/edit?usp=sharing")
+
+    # Select a specific worksheet within the spreadsheet
+    worksheet = sheet.get_worksheet(0)  # Index 0 corresponds to the first worksheet
+
+    # Read data from the worksheet
+    all_values = worksheet.get_all_values()
+
+    # Display the read data
+    for row in all_values:
+        print(row)
 
 if __name__ == "__main__":
     main()
